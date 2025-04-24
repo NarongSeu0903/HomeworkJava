a3e7bf7fe87142041d3a3d798a91dc85481af27f
NarongSeu0903 <narongseu20@gmail.com>
2025-04-24 16:03:11 +0700

    First Push

--- src/Homework.java
+++ src/Homework.java
@@ -1,7 +1,8 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Homework {
-    public static int wordCount(String input) {
+    private static int wordCount(String input) {
         if (input == null || input.trim().isEmpty()) {
             return 0;
         }
@@ -9,21 +10,16 @@ public class Homework {
         return words.length;
     }
 
-    public static int[] sort(int[] numbers) {
+    private static int[] sort(int[] numbers) {
         Arrays.sort(numbers);
         return numbers;
     }
     public static void main(String[] args) {
-
         String sentence = "Hello world, this is Java!";
         int count = wordCount(sentence);
-        System.out.println("Word count: " + count);
-
-        int[] nums = {100, 400, 600, 8, 2};
-        int[] sorted = sort(nums);
-        System.out.print("Sorted array: ");
-        for (int num : sorted) {
-            System.out.println(num + "");
-        }
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Typing of word: " +count);
+        String wordTyping = sc.nextLine();
+        System.out.println("Word count: " + wordTyping);
     }
 }
